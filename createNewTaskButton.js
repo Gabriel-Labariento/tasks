1427ac8d49e659c416b28ea455979ceaa9f376be
Gabriel-Labariento <gabrielmatthew.labariento@gmail.com>
2025-01-29 19:59:55 +0800

    Converted html template to js.

---
+++ createNewTaskButton.js
@@ -0,0 +1,14 @@
+const createNewTaskButton = function(){
+    const newTaskContainer = document.createElement('div');
+    newTaskContainer.className = 'new-task-container';
+    
+    const newTaskButton = document.createElement('button');
+    newTaskButton.className = 'new-task-button col';
+    newTaskButton.textContent = '+ New';
+
+    newTaskContainer.appendChild(newTaskButton);
+
+    return newTaskContainer;
+}
+
+export {createNewTaskButton}
